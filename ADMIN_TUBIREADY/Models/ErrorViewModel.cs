24cec0ae52b56afe263722dd05c7667b0874992d
H4d3ssss <francisco.ralffieheart.doctolero@gmail.com>
2025-11-18 21:59:40 +0800

    Add project files.

---
+++ ADMIN_TUBIREADY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADMIN_TUBIREADY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ADMIN_TUBIREADY/Residents.vb
@@ -0,0 +1,6 @@
+﻿Partial Public Class Residents
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+End Class
